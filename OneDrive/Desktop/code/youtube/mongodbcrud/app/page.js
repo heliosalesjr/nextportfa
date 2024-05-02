ce22b07aab64755a8a55974ac73d9bac85344f83
heliosalesjr <heliosalesjr@gmail.com>
2024-05-02 15:18:22 -0300

    here comes the CRUD

---
+++ OneDrive/Desktop/code/youtube/mongodbcrud/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <h1 className='text-center text-5xl text-pink-500 py-8'>Hola</h1>
+    <p className='text-center text-3xl text-green-500 font-semibold'>soy un CRUD!</p>
+    </>
+  )
+}
+
+export default Home
